596f44099c156c35a97a74bc85bf544d83cdea05
Akiel <122918848+Akieldamas@users.noreply.github.com>
2023-11-23 15:47:33 +0100

    V1

--- WeatherView/MainWindow.xaml.cs
+++ WeatherView/MainWindow.xaml.cs
@@ -13,16 +13,113 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Net.Http;
+using Newtonsoft.Json;
+
 namespace WeatherView
 {
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+
+    public class Root
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        _: GetWeather();
         }
+
+        public async Task<string> GetWeather()
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=annecy,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric");
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    Root root = JsonConvert.DeserializeObject<Root>(content);
+
+                    Main main = root.main;
+                    TB_Currently.Text = "Today:  " + main.temp.ToString() + "°C";
+                    TB_High.Text = "High:  " + root.main.temp_max.ToString() + "°C";
+                    TB_Low.Text = "Low:  " + main.temp_min.ToString() + "°C";
+                    TB_Feels.Text = "Feels:  " + main.feels_like.ToString() + "°C";
+                    TB_WeatherState.Text = root.weather[0].description.ToString();
+                    TB_Humidity.Text = "Precipitation: " + main.humidity.ToString() + " %";
+                    return "";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return null;
+        }
+
     }
 }
