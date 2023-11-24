bd5c931b8c0880b25d1e9a2eac838e23966aca77
Akieldamas <122918848+Akieldamas@users.noreply.github.com>
2023-11-24 16:25:38 +0100

    WIP

--- WeatherView/MainWindow.xaml.cs
+++ WeatherView/MainWindow.xaml.cs
@@ -86,40 +86,42 @@ namespace WeatherView
     /// </summary>
     public partial class MainWindow : Window
     {
+        
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        _: GetWeather();
-        }
+        /*  public MainWindow()
+          {
+              InitializeComponent();
+          _: GetWeather();
+          }
 
-        public async Task<string> GetWeather()
-        {
-            HttpClient client = new HttpClient();
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=annecy,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric");
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    Root root = JsonConvert.DeserializeObject<Root>(content);
+          public async Task<string> GetWeather()
+          {
+              HttpClient client = new HttpClient();
+              try
+              {
+                  HttpResponseMessage response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather?q=annecy,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric");
+                  if (response.IsSuccessStatusCode)
+                  {
+                      var content = await response.Content.ReadAsStringAsync();
+                      Root root = JsonConvert.DeserializeObject<Root>(content);
 
-                    Main main = root.main;
-                    TB_Currently.Text = "Today:  " + main.temp.ToString() + "°C";
-                    TB_High.Text = "High:  " + root.main.temp_max.ToString() + "°C";
-                    TB_Low.Text = "Low:  " + main.temp_min.ToString() + "°C";
-                    TB_Feels.Text = "Feels:  " + main.feels_like.ToString() + "°C";
-                    TB_WeatherState.Text = root.weather[0].description.ToString();
-                    TB_Humidity.Text = "Precipitation: " + main.humidity.ToString() + " %";
-                    return "";
-                }
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-            return null;
-        }
+                      Main main = root.main;
+                      TB_Currently.Text = "Today:  " + main.temp.ToString() + "°C";
+                      TB_High.Text = "High:  " + root.main.temp_max.ToString() + "°C";
+                      TB_Low.Text = "Low:  " + main.temp_min.ToString() + "°C";
+                      TB_Feels.Text = "Feels:  " + main.feels_like.ToString() + "°C";
+                      TB_WeatherState.Text = root.weather[0].description.ToString();
+                      TB_Humidity.Text = "Precipitation: " + main.humidity.ToString() + " %";
+                      return "";
+                  }
+              }
+              catch (Exception ex)
+              {
+                  return ex.Message;
+              }
+              return null;
+          }
 
+      */
     }
 }
